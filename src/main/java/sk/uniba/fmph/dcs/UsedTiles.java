5a4c141d33d4ffa33b53d87cfdb24d425425cee6
champion254 <84034909+champion254@users.noreply.github.com>
2023-11-15 18:39:24 +0100

    Implementacia UsedTiles

---
+++ src/main/java/sk/uniba/fmph/dcs/UsedTiles.java
@@ -0,0 +1,27 @@
+package sk.uniba.fmph.dcs;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class UsedTiles implements UsedTilesGiveInterface{
+    private List<Tile> tiles;
+    public UsedTiles() {
+        tiles = new ArrayList<>();
+    }
+    public void give(Collection<Tile> tile) {
+        tiles.addAll(tile);
+    }
+    public Collection<Tile> takeAll(){
+        List<Tile> tempTiles = new ArrayList<>(tiles);
+        tiles.clear();
+        return tempTiles;
+    }
+    public String state(){
+        String toReturn = "";
+        for (final Tile tile : tiles) {
+            toReturn += tile.toString();
+        }
+        return toReturn;
+    }
+}
