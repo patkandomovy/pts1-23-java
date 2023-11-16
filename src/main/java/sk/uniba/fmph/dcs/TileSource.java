14488827beff9e2fdd03eb8c51600a6ec9d7aed3
champion254 <84034909+champion254@users.noreply.github.com>
2023-11-16 21:28:12 +0100

    Uprava TileSource

--- src/main/java/sk/uniba/fmph/dcs/TileSource.java
+++ src/main/java/sk/uniba/fmph/dcs/TileSource.java
@@ -4,33 +4,24 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public abstract class TileSource {
-    private ArrayList<Tile> tiles;
+    protected ArrayList<Tile> tiles;
 
-    public TileSource() {
+    protected TileSource() {
         tiles = new ArrayList<>();
     }
 
-    public Collection<Tile> take(Tile tileType) {
-        Collection<Tile> takenTiles = new ArrayList<>();
-        for (Tile tile : tiles) {
-            if (tile == tileType) {
-                takenTiles.add(tile);
-            }
-        }
-        tiles.removeAll(takenTiles);
-        return takenTiles;
-    }
+    public abstract Collection<Tile> take(Tile tileType);
 
     public boolean isEmpty() {
         return tiles.isEmpty();
     }
-
-    public void startNewRound() {
-        //TODO
-    }
+    public abstract void startNewRound();
 
     public String state() {
-        //TODO
-        return null;
+        String toReturn = "";
+        for (final Tile tile : tiles) {
+            toReturn += tile.toString();
+        }
+        return toReturn;
     }
 }
