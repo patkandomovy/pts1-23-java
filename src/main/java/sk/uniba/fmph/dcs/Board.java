5954be2ea28dee2559fb9021dc021cb0f782c655
matusbucher <bucher.matus@gmail.com>
2023-11-22 20:26:59 +0100

    Added Board class without implementation of methods

---
+++ src/main/java/sk/uniba/fmph/dcs/Board.java
@@ -0,0 +1,24 @@
+package sk.uniba.fmph.dcs;
+
+import java.util.List;
+
+public class Board {
+    private Points points;
+
+    public void put(int destinationIdx, List<Tile> tiles) {
+        // TODO:
+    }
+
+    public FinishRoundResult finishRound() {
+        // TODO:
+        return null;
+    }
+
+    public void endGame() {
+        // TODO:
+    }
+
+    public String state() {
+        return null;
+    }
+}
