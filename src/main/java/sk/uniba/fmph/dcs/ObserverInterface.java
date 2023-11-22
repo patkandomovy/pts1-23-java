d72f8064aa7d55583a9ca0337609065b26422c9d
matusbucher <bucher.matus@gmail.com>
2023-11-22 19:05:15 +0100

    zmena implementacie ObserverInterface

--- src/main/java/sk/uniba/fmph/dcs/ObserverInterface.java
+++ src/main/java/sk/uniba/fmph/dcs/ObserverInterface.java
@@ -1,7 +1,5 @@
 package sk.uniba.fmph.dcs;
 
 interface ObserverInterface {
-    void registerObserver(ObserverInterface observer);
-    void cancelObserver(ObserverInterface observer);
-    void notify(String newState);
+    void update();
 }
